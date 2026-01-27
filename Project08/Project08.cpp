31f9a4a0220d5f906b2217d509f8d3dd34691c72
hcb <cheabinhan8281@gmail.com>
2026-01-27 12:51:44 +0900

    26.01.27

--- Project08/Project08.cpp
+++ Project08/Project08.cpp
@@ -1,4 +1,4 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #define COUNT   5       // 매크로 상수
 
@@ -8,6 +8,7 @@ int main()
     const int NUMBER = 5;   // 상수 정의
     */
 
+    /*
     int i = 0;
     while (i < COUNT) {
         int j = 0;
@@ -17,7 +18,49 @@ int main()
         } 
         printf("\n");
         i++;
-    }
+     
+    }*/
+
+    //do while();       # 반복문 안의 명령문을 적어도 한번 실행
+                       // while() 뒤에 반드시 ; 붙여줌
+    char ch, tmp;
+    do
+    {
+        printf("1. 새파일\n");
+        printf("2. 저장\n");
+        printf("3. 열기\n");
+        printf("=============\n");
+        printf("x. 종료\n");
+
+        printf("\n메뉴를 선택해주세요: ");
+        scanf("%c", &ch);           // %c -> 문자 1 ('1')로 인식
+        scanf("%c", &tmp);          // 입력버퍼 초기화
+        
+        switch (ch)
+        {
+        case '1':
+            printf("\n새 파일이 선택되었습니다.\n");
+            break;
+        case '2':
+            printf("\n저장이 선택되었습니다.");
+            break;
+        case '3':
+            printf("\n열기가 선택되었습니다.");
+            break;
+        case 'x':break;
+        default:
+            printf("\n잘못입력했습니다. 다시 입력해주세요.");
+            break;
+
+        }
+
+         /* switch 대용 if문
+         if (ch == '1') printf("~~")
+         else if (ch == '2')
+         */
+
+    } while (ch!='x');
+
 
 
     return 0;
---
+++ Project10/Project10.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    // 사칙연산을 수행하는 계산기 프로그램 작성
+    // 두개의 정수와 연산자를 입력받아 연산 수행 및 결과 출력
+    
+    int num1, num2, res;
+    char mark, ch, tmp;
+    do
+    {
+        printf("\n사칙연산 프로그램  ");
+        
+
+        printf("\n첫번째 정수를 입력하세요: ");
+        scanf("%d", &num1);
+        scanf("%c", &tmp);
+
+        printf("\n다음 연산자 중 하나를 입력하세요 (+, -, *, /) (x 입력시 종료): ");
+        scanf("%c", &mark);
+        scanf("%c", &tmp);
+        if (mark == 'x') break;
+
+        printf("\n두번째 정수를 입력하세요: ");
+        scanf("%d", &num2);
+
+        switch (mark)
+        {
+        case '+':
+            printf("%d + %d = %d", num1, num2, num1+num2);
+            break;
+        case '-':
+            printf("%d - %d = %d", num1,num2, num1-num2);
+            break;
+        case '*':
+            printf("%d * %d = %d", num1, num2, num1*num2);
+            break;
+        case '/':
+            printf("%d / %d = %d", num1, num2, num1/num2);
+            break;
+        case 'x': 
+            printf("x 입력시 종료");
+            break;
+        default:
+            break;
+        }
+
+        
+    } while (mark != 'x'); 
+
+
+    return 0;
+}
+
