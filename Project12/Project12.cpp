dacbfad68a61aa0c59b17f57f55bf00af732af35
hcb <cheabinhan8281@gmail.com>
2026-01-29 12:51:26 +0900

    26.01.29

--- Project12/Project12.cpp
+++ Project12/Project12.cpp
@@ -40,40 +40,40 @@ int main()
     }*/
 
     // 배열의 메모리 할당 크기
-    printf("int형의 크기: %llubytes\n", sizeof(int));
-    printf("배열 arr의 크기: %llubytes\n", sizeof(arr));    
-    printf("배열 nums의 크기: %llubytes\n", sizeof(nums));
-    printf("배열 sz의 크기: %llubytes\n", sizeof(sz));
-    printf("배열 my_nums의 크기: %llubytes\n", sizeof(my_nums));
-    printf("배열 bigs의 크기: %llubytes\n", sizeof(bigs));
+    //printf("int형의 크기: %llubytes\n", sizeof(int));
+    //printf("배열 arr의 크기: %llubytes\n", sizeof(arr));    
+    //printf("배열 nums의 크기: %llubytes\n", sizeof(nums));
+    //printf("배열 sz의 크기: %llubytes\n", sizeof(sz));
+    //printf("배열 my_nums의 크기: %llubytes\n", sizeof(my_nums));
+    //printf("배열 bigs의 크기: %llubytes\n", sizeof(bigs));
 
-    arr[0] = 88;
-    arr[1] = 95;
-    arr[2] = 100;
-    arr[3] = 70;
-    arr[4] = 65;
+    //arr[0] = 88;
+    //arr[1] = 95;
+    //arr[2] = 100;
+    //arr[3] = 70;
+    //arr[4] = 65;
 
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                // 두 값 서로 교환
-                int tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-            }
-                
-        }
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d\t", arr[i]);
-    }
+    //for (int i = 0; i < 5; i++)
+    //{
+    //    for (int j = 0; j < 4; j++)
+    //    {
+    //        if (arr[j] > arr[j + 1])
+    //        {
+    //            // 두 값 서로 교환
+    //            int tmp = arr[j];
+    //            arr[j] = arr[j + 1];
+    //            arr[j + 1] = tmp;
+    //        }
+    //            
+    //    }
+    //}
+    //for (int i = 0; i < 5; i++)
+    //{
+    //    printf("%d\t", arr[i]);
+    //}
 
     // arr 배열의 값들의 합과 평균
-    int total = 0;
+    /*int total = 0;
     for (int i = 0; i < 5; i++)
     {
         printf("arr[%d] =", i);
@@ -81,12 +81,27 @@ int main()
         total += arr[i];
     }
     float average = total / (sizeof(arr) / sizeof(int));
-    printf("값: %d, 평균: %.2f", total, average);
+    printf("값: %d, 평균: %.2f", total, average);*/
 
 
     // int(4byte) *5 = arr[5] (20byte)
     // 배열 크기 = 배열 타입의 크기 * 배열의 길이
     // 정적배열: 배열 크기 고정(<-> 동적배열)
+
+    //이 배열의 아이템 개수 출력
+    short numbers[] = { 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9 };
+    short length = sizeof(numbers) / sizeof(short);
+    printf("numbers의 아이템 개수: %hd\n", length);
+    // 마지막 배열에 100 대입 후 마지막 값 출력
+    numbers[length-1] = 100;
+    printf("numbers[%hd] = %hd\n\n", length-1, numbers[length-1]);
+
+    //for 문을 이용해 numbers 배열에 1~ 개수까지의 수를 순서대로 대입
+    for (int i = 0; i < length; i++)
+    {
+        numbers[i] = i + 1;
+        printf("numbers[%d] = %d\n", i, numbers[i]);
+    }
 
 
 
--- Project13/Project13.cpp
+++ Project13/Project13.cpp
@@ -1,32 +1,53 @@
-﻿// Project13.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>		// Standard Input/Output 표준 입출력 관련 함수
+#include <string.h>		// 문자열 처리에 관련된 함수
 int main()
 {
-	char sz[] = { 65,66,67,68,69,70,0 };
-	int len = sizeof(sz) / sizeof(char);		// 배열의 길이
+	//char sz[] = { 65,66,67,68,69,70,0 };
+	//int len = sizeof(sz) / sizeof(char);		// 배열의 길이
 
-	for (int i = 0; i < len; i++)
-	{
-		printf("%c", sz[i]);
-	}
+	//for (int i = 0; i < len; i++)
+	//{
+	//	printf("%c", sz[i]);
+	//}
 
 	// 배열로 문자열 만들기: 배열 끝에 0 추가.
-	// 문자열은 배열 
-	// 0 == '\0'
-	// char sz[] = {'A','B','C','D','E','F','\0'};
-	printf("%s\n", sz);
+	// 문자열 == 배열 
+	// 0 == '\0'	null terminated character(문자열 마감)
+	// char sz[] = {'A','B','C','D','E','F','\0'};			// 작은 따옴표'' 안에 한 문자만 가능!
+	//char sz[] = { 'H', 'e','l','l','o' ,'\0'};
+	char sz[] = "Hello";		// 문자열 (string)
+	//int len = sizeof(sz);
+	//printf("sz배열의 크기: %d\n", len);			// 6
+	//printf("%s\n", sz);
+	//sz[1] = sz[1] - 32;
+	//printf("%s\n", sz);
+
+	// 배열의 크기와 문자열의 길이
+	// 문자열의 길이
+	int slen = strlen(sz);
+	printf("sz문자열의 길이: %d\n", slen);		// 5
+
+	char sz2[] = "Hello";
+	// 문자열 비교
+	//if (sz == sz2)			// 잘못된 비교
+	if(strcmp(sz, sz2) == 0)	// 옳은 비교
+		printf("%s와 %s는 서로 같음\n", sz, sz2);
+	else
+		printf("%s와 %s는 서로 다름\n", sz, sz2);
+	printf("%d \n%d\n", &sz, &sz2);
+
+	// 문자열의 입력
+	char name[20];			// 알파벳 9글자 + \0  입력가능
+	printf("이름을 입력하세요: ");
+	//scanf("%s", name);		// 문자열 입력 %s, 배열의 경우: &(주소연산자) 사용X, 공백 기준으로 입력 끊김
+	//printf("%s", name);
+
+	//fgets(받을 배열, 최대길이, stdin)			// 공백 있어도 입력 끊기지 않음
+	//표준입력장치로부터 문자열을 입력받아 name에 대입
+	fgets(name, 20, stdin);
+	printf("%s", name);
+
+
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
